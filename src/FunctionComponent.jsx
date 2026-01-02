b31cf8720cee080762e5875f2f6a8a679752fe5e
amitbhosale1137 <amitbhosale1137@gmail.com>
2026-01-02 13:53:28 +0530

    arrow function

--- src/FunctionComponent.jsx
+++ src/FunctionComponent.jsx
@@ -3,10 +3,15 @@ function EventCalling(){
     function callFun(){
         alert('function called')
     }
+
+    const fruit=()=>{
+        alert('apple')
+    }
+
     return(
         <div>
             <h1>Event and Function call</h1>
-            <button onClick={callFun}>Click Me</button>
+            <button onClick={fruit}>Click Me</button>
         </div>
     )
 }
