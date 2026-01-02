db33d6a046c4b34e2bad6fbbe70aa18807a91d51
amitbhosale1137 <amitbhosale1137@gmail.com>
2026-01-02 13:25:10 +0530

    sum function calling

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ function App() {
     return 'Apple'
   }
 
+  function sum(a,b){
+    return a+b
+  }
+
   return (
     <>
       <h1>React 19 Version</h1>
@@ -22,6 +26,7 @@ function App() {
       <h1>{name}</h1>  
       Z : {x+y} 
       <h1>{fruit()}</h1>
+      <h1>{sum(10,30)}</h1>
     </>
   )
 }
