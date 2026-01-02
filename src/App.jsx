654c1cc13a950980721983981b5d6ad3adafb25a
amitbhosale1137 <amitbhosale1137@gmail.com>
2026-01-02 13:21:10 +0530

    use of variables

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ import Login, { Profile } from './UserComponent'
 
 function App() {
 
+  const name ='Amit Bhosale'
+  let x = 10
+  let y = 20
+
   return (
     <>
       <h1>React 19 Version</h1>
       <Header/>
       <Login/>
-      <Profile/>      
+      <Profile/>
+      <h1>{name}</h1>  
+      Z : {x+y}  
     </>
   )
 }
